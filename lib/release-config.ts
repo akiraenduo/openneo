4272a4a035cf839fdae994c2c15fcd06a6074067
Akira Endo <akira.endo@langdemy.com>
2026-02-22 21:58:36 +0900

    fixing

--- lib/release-config.ts
+++ lib/release-config.ts
@@ -1,6 +1,5 @@
 export const RELEASE_VERSION = '0.8.0'
-export const GITHUB_REPO = 'anthropics/openneo'
 
 export function getDownloadUrl(version: string = RELEASE_VERSION): string {
-  return `https://github.com/${GITHUB_REPO}/releases/download/v${version}/OpenNeo-${version}-arm64.dmg`
+  return `/downloads/OpenNeo-${version}-arm64.dmg`
 }
