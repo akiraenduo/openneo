49104f8fba421dd7af5326549e0c2c3207e31da2
Akira Endo <akira.endo@langdemy.com>
2026-02-22 22:13:28 +0900

    fixing

--- lib/release-config.ts
+++ lib/release-config.ts
@@ -1,5 +1,6 @@
-export const RELEASE_VERSION = '0.8.0'
+export const RELEASE_VERSION = '0.1.0'
+export const STORAGE_BUCKET = 'open-neo.firebasestorage.app'
 
 export function getDownloadUrl(version: string = RELEASE_VERSION): string {
-  return `/downloads/OpenNeo-${version}-arm64.dmg`
+  return `https://${STORAGE_BUCKET}/installers/OpenNeo-${version}-arm64.dmg`
 }
