150a96057a6001ae8353aedef60683511f0fe26e
Akira Endo <akira.endo@langdemy.com>
2026-02-22 22:16:25 +0900

    fixing

--- lib/release-config.ts
+++ lib/release-config.ts
@@ -2,5 +2,6 @@ export const RELEASE_VERSION = '0.1.0'
 export const STORAGE_BUCKET = 'open-neo.firebasestorage.app'
 
 export function getDownloadUrl(version: string = RELEASE_VERSION): string {
-  return `https://${STORAGE_BUCKET}/installers/OpenNeo-${version}-arm64.dmg`
+  const path = encodeURIComponent(`installers/OpenNeo-${version}-arm64.dmg`)
+  return `https://firebasestorage.googleapis.com/v0/b/${STORAGE_BUCKET}/o/${path}?alt=media`
 }
